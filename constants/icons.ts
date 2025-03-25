c7e6dd356246077813b5d32522c4d7cd356ef82a
glish0 <dylanemempouza02@gmail.com>
2025-03-25 12:52:17 +0100

    icons call configured

---
+++ constants/icons.ts
@@ -0,0 +1,13 @@
+const home = require("../assets/icons/home.png");
+const grades = require("../assets/icons/grades.png");
+const schedule = require("../assets/icons/schedule.png");
+const messages = require("../assets/icons/messages.png");
+const book = require("../assets/icons/book.png");
+
+export default {
+  home,
+  grades,
+  schedule,
+  messages,
+  book
+};
