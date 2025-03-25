bbfc89e75366ed4ae6c4c912b37335225581177a
glish0 <dylanemempouza02@gmail.com>
2025-03-25 12:52:46 +0100

    images call configured

---
+++ constants/images.ts
@@ -0,0 +1,13 @@
+const slide1 = require("../assets/images/slide1.png");
+const slide2 = require("../assets/images/slide2.png");
+const slide3 = require("../assets/images/slide3.png");
+const logo = require("../assets/images/logo.svg");
+const background = require("../assets/images/background.svg");
+
+export default {
+  slide1,
+  slide2,
+slide3,
+logo,
+background
+};
