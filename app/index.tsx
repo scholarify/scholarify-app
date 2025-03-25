6c60b7f38a3f277bd8e604f277f8bb1c439a4da5
glish0 <dylanemempouza02@gmail.com>
2025-03-25 10:32:15 +0100

    font and color config test

--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <View className='flex-1 items-center justify-center bg-white' >
-      <Text className='text-3xl font-bold'>Scholarify</Text>
+    <View className='flex-1 items-center justify-center bg-bg-secondary-navy' >
+      <Text className='text-3xl font-bbold text-text-primary'>Scholarify</Text>
     </View>
   )
 }
