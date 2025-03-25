5e821771abfe23b87f8c94e998e9b6158e6e31d7
glish0 <dylanemempouza02@gmail.com>
2025-03-25 10:31:20 +0100

    font config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Slot, Stack } from 'expo-router'
+import React, { useEffect } from 'react'
+import { Slot, SplashScreen, Stack } from 'expo-router'
 import "@/global.css"
+import { useFonts } from 'expo-font'
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "BricolageGrotesque-Regular": require("../assets/fonts/BricolageGrotesque-Regular.ttf"),
+    "BricolageGrotesque-Bold": require("../assets/fonts/BricolageGrotesque-Bold.ttf"),
+    "BricolageGrotesque-SemiBold": require("../assets/fonts/BricolageGrotesque-SemiBold.ttf"),
+  })
+
+  useEffect(() => {
+    if(error) throw error;
+
+    if(fontsLoaded) SplashScreen.hideAsync()
+   
+  }, [fontsLoaded, error] )
+  if(!fontsLoaded && !error) return null;
+
   return (
     <Stack>
       <Stack.Screen name='index' options={{headerShown: true}} />
